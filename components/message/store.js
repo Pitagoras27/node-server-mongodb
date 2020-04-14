21e75e62ad5efb1bb83cb9a47cc856528492bdd8
Carlos Gutierrez <elimperiodeloefimero@gmail.com>
2020-04-13 21:28:41 -0500

    Se crea base de datos en MongoDBAtlas; se instala mongoose; se genera su modelo y se refactoriza el store para consumir generar y consumir data real

--- components/message/store.js
+++ components/message/store.js
@@ -1,11 +1,23 @@
-const list = [];
+const db = require('mongoose')
+const Model = require('./model')
+
+db.Promise = global.Promise
+db.connect('mongodb+srv://<user_name>:<password>@cluster0-gnypt.mongodb.net/<db_name>', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
+console.log('Connected successful!')
 
 const addMessage = (message) => {
-    list.push(message);
+    // list.push(message);
+    const myMessage = new Model(message)
+    myMessage.save()
 }
 
-const getMessages = () => {
-    return list;
+const getMessages = async () => {
+    const messages = await Model.find()
+    return messages
 }
 
 module.exports = {
